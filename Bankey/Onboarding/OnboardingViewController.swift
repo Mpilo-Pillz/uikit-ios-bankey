68b24f95e37e9282cda073f3a55adbe49de6808d
mpilopillz <mpilo.dlamini@yahoo.com>
2022-09-03 14:43:51 +0200

    adding onboaridng view controller

---
+++ Bankey/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  OnboardingViewController.swift
+//  Bankey
+//
+//  Created by Mpilo Pillz on 2022/09/03.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    let stackView = UIStackView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "mub"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(label)
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
