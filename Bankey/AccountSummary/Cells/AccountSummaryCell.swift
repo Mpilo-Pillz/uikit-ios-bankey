239550665e74f47a3136eb913668f8eaf5f5513d
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-10 09:28:43 +0200

    adding table view cell boiler plate

---
+++ Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -0,0 +1,31 @@
+//
+//  AccountSummaryCell.swift
+//  Bankey
+//
+//  Created by Mpilo Pillz on 2022/11/10.
+//
+
+import Foundation
+import UIKit
+
+class AccountSummaryCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension AccountSummaryCell {
+    private func setup() {
+        
+    }
+    
+    private func layout() {
+        
+    }
+}
