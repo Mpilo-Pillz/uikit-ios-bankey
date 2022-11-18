243f298423db39aaaf4c7d9524b72e8fd3bb55e9
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-18 10:12:00 +0200

    test - setting up unit tests

---
+++ BankeyUnitTests/AccountSummary/AccountSummaryViewControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  AccountSummaryViewControllerTests.swift
+//  BankeyUnitTests
+//
+//  Created by Mpilo Pillz on 2022/11/19.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankey
+
+class AccountSummaryViewControllerTests: XCTestCase {
+    var vc: AccountSummaryViewController!
+    
+    override func setUp() {
+        super.setUp()
+        vc = AccountSummaryViewController()
+//        vc.loadViewIfNeeded()
+    }
+    
+    func testSomething() throws {
+        
+    }
+}
